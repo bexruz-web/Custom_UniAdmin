fdc16992d3b8b9e4d98975b767ffd9d4a8d5c5a3
bexruz <bexruzasqarov08@gmail.com>
2025-01-07 22:25:57 +0500

    services.py has been completed

---
+++ adminapp/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from .models import *
+
+
+class FacultyForm(forms.ModelForm):
+    class Meta:
+        model = Faculty
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class KafedraForm(forms.ModelForm):
+    class Meta:
+        model = Kafedra
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class SubjectsForm(forms.ModelForm):
+    class Meta:
+        model = Subjects
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class TeachersForm(forms.ModelForm):
+    class Meta:
+        model = Teachers
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class GroupsForm(forms.ModelForm):
+    class Meta:
+        model = Groups
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class StudentsForm(forms.ModelForm):
+    class Meta:
+        model = Students
+        fields = "__all__"
+        widgets = {
+            "name": forms.TextInput(attrs={'class': 'form-control'})
+        }
