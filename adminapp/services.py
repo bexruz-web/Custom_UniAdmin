b566d17e805fd84dc75fad34c5d900c7e2eba1c6
bexruz <bexruzasqarov08@gmail.com>
2025-01-09 14:29:54 +0500

    funtion to get kafedras

--- adminapp/services.py
+++ adminapp/services.py
@@ -24,3 +24,14 @@ def get_data_from_table(table_name):
         cursor.execute(f"SELECT * FROM {table_name}")
         # Jadvaldagilarni lug'at shaklida qaytaradi
         return dictfetchall(cursor)
+
+
+# adminapp_kafedra jadvaliga adminapp_faculty dan faculty nomlarini olish uchun LEFT JOIN ishlatildi
+def get_kafedra_with_faculty():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""
+            SELECT adminapp_kafedra.*, adminapp_faculty.name AS faculty_name
+            FROM adminapp_kafedra
+            LEFT JOIN adminapp_faculty ON adminapp_kafedra.faculty_id = adminapp_faculty.id
+        """)
+        return dictfetchall(cursor)
