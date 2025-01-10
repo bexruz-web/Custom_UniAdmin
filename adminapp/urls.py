663eb629bdb5de7582fd546973847239d0b68097
bexruz <bexruzasqarov08@gmail.com>
2025-01-11 02:01:01 +0500

    urls.py has been completed

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -20,5 +20,20 @@ urlpatterns = [
     path('subject/<int:pk>/edit/', subject_edit, name='subject_edit'),
     path('subject/<int:pk>/delete', subject_delete, name='subject_delete'),
     path('subjects/list/', subjects_list, name='subjects_list'),
+    # TEACHERS
+    path('teacher/create/', teacher_create, name='teacher_create'),
+    path('teacher/<int:pk>/edit', teacher_edit, name='teacher_edit'),
+    path('teacher/<int:pk>/delete/', teacher_delete, name='teacher_delete'),
+    path('teachers/list/', teachers_list, name='teachers_list'),
+    # GROUPS
+    path('group/create/', group_create, name='group_create'),
+    path('group/<int:pk>/edit', group_edit, name='group_edit'),
+    path('group/<int:pk>/delete', group_delete, name='group_delete'),
+    path('groups/list/', groups_list, name='groups_list'),
+    # STUDENTS
+    path('student/create/', student_create, name='student_create'),
+    path('student/<int:pk>/edit/', student_edit, name='student_edit'),
+    path('student/<int:pk>/delete/', student_delete, name='student_delete'),
+    path('student/list/', student_list, name='students_list'),
 
 ]
