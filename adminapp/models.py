9ce743fde7c2952d70c4a234ac57153fb69bb089
bexruz <bexruzasqarov08@gmail.com>
2025-01-13 23:10:34 +0500

    updated

--- adminapp/models.py
+++ adminapp/models.py
@@ -27,6 +27,7 @@ class Teachers(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50)
     subjects = models.ForeignKey(Subjects, on_delete=models.CASCADE, related_name="teachers")
+    kafedra = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -34,10 +35,7 @@ class Teachers(models.Model):
 
 class Groups(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     kafedra = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
-    subjects = models.ManyToManyField(Subjects, related_name="groups")
-    year = models.IntegerField()
     mentor = models.ForeignKey(Teachers, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
