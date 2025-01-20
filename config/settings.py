7bf380ac4f77e7c2f3f953194b4a889445879c63
bexruz <bexruzasqarov08@gmail.com>
2025-01-21 01:21:54 +0500

    URLs were set up for working with media

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
