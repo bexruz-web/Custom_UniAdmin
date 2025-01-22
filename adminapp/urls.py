6ba3a2d5362a9ecb80ab2da42ec850bbc56580e0
bexruz <bexruzasqarov08@gmail.com>
2025-01-22 22:01:53 +0500

    new url added

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -35,5 +35,7 @@ urlpatterns = [
     path('student/<int:pk>/edit/', student_edit, name='student_edit'),
     path('student/<int:pk>/delete/', student_delete, name='student_delete'),
     path('student/list/', student_list, name='students_list'),
+    # PROFILE
+    path('profile', profile, name='profile')
 
 ]
